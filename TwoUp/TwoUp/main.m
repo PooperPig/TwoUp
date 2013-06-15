f0db1febcc02911ef49572c614debd6ebed0dc73
Maxxx <mailmaxxx@gmail.com>
2013-06-15 21:23:50 +1000

    Initial Commit

---
+++ TwoUp/TwoUp/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TwoUp
+//
+//  Created by Max Young on 15/06/13.
+//  Copyright PooperPig 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
