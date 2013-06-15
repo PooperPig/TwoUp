c0fe6b1a17a10f392dfb0fcd2a693bbb29588c48
Maxxx <mailmaxxx@gmail.com>
2013-06-15 21:52:02 +1000

    Completed initial changes and added sprites

--- TwoUp/TwoUp/HelloWorldLayer.mm
+++ TwoUp/TwoUp/HelloWorldLayer.mm
@@ -1,88 +1,64 @@
 //
 //  HelloWorldLayer.mm
-//  TwoUp
+//  Fifty50
 //
-//  Created by Max Young on 15/06/13.
-//  Copyright PooperPig 2013. All rights reserved.
+//  Created by Maxxx on 11/06/13.
+//  Copyright Maxxx 2013. All rights reserved.
 //
 
-// Import the interfaces
 #import "HelloWorldLayer.h"
-
-// Not included in "cocos2d.h"
 #import "CCPhysicsSprite.h"
-
-// Needed to obtain the Navigation Controller
-#import "AppDelegate.h"
-
-
-enum {
-	kTagParentNode = 1,
-};
-
-
-#pragma mark - HelloWorldLayer
 
 @interface HelloWorldLayer()
 -(void) initPhysics;
 -(void) addNewSpriteAtPosition:(CGPoint)p;
--(void) createMenu;
 @end
+float timeBetweenFlips = 2.0;
+float timeUntilNextFlip = -1;
+int numberOfHeads = 0;
+int numberOfTails = 0;
+int numberOfFlips = 10;
+CCLabelTTF *headsLabel;
+CCLabelTTF *tailsLabel;
+CCLabelTTF *resultLabel;
+
+bool paused = false;
 
 @implementation HelloWorldLayer
 
 +(CCScene *) scene
 {
-	// 'scene' is an autorelease object.
 	CCScene *scene = [CCScene node];
-	
-	// 'layer' is an autorelease object.
 	HelloWorldLayer *layer = [HelloWorldLayer node];
-	
-	// add layer as a child to scene
 	[scene addChild: layer];
-	
-	// return the scene
 	return scene;
 }
 
 -(id) init
 {
+	//!! Use braces on same line or next line indiscriminately
 	if( (self=[super init])) {
-		
-		// enable events
-		
+		//!! Make sure we use both dod access and 'traditional' square-bracket access to properties
 		self.touchEnabled = YES;
-		self.accelerometerEnabled = YES;
+		[self setAccelerometerEnabled: YES];
 		CGSize s = [CCDirector sharedDirector].winSize;
-		
-		// init physics
-		[self initPhysics];
-		
-		// create reset button
-		[self createMenu];
-		
-		//Set up sprite
-		
-#if 1
-		// Use batch node. Faster
-		CCSpriteBatchNode *parent = [CCSpriteBatchNode batchNodeWithFile:@"blocks.png" capacity:100];
+
+		CCSpriteBatchNode *parent = [CCSpriteBatchNode batchNodeWithFile:@"sprites.png" capacity:100];
 		spriteTexture_ = [parent texture];
-#else
-		// doesn't use batch node. Slower
-		spriteTexture_ = [[CCTextureCache sharedTextureCache] addImage:@"blocks.png"];
-		CCNode *parent = [CCNode node];
-#endif
-		[self addChild:parent z:0 tag:kTagParentNode];
-		
-		
-		[self addNewSpriteAtPosition:ccp(s.width/2, s.height/2)];
-		
-		CCLabelTTF *label = [CCLabelTTF labelWithString:@"Tap screen" fontName:@"Marker Felt" fontSize:32];
-		[self addChild:label z:0];
-		[label setColor:ccc3(0,0,255)];
-		label.position = ccp( s.width/2, s.height-50);
-		
+		[self addChild:parent z:0 ];
+
+		[self initPhysics];
+
+		headsLabel = [CCLabelTTF labelWithString:@"Heads:" fontName:@"Marker Felt" fontSize:32];
+		[self addChild:headsLabel z:0];
+		[headsLabel setColor:ccc3(0,0,255)];
+		headsLabel.position = ccp( 60, s.height-30);
+
+		tailsLabel = [CCLabelTTF labelWithString:@"Tails:" fontName:@"Marker Felt" fontSize:32];
+		[self addChild:tailsLabel z:0];
+		[tailsLabel setColor:ccc3(0,0,255)];
+		tailsLabel.position = ccp( s.width - 80, s.height-30);
+
 		[self scheduleUpdate];
 	}
 	return self;
@@ -92,122 +68,98 @@ enum {
 {
 	delete world;
 	world = NULL;
-	
+
 	delete m_debugDraw;
 	m_debugDraw = NULL;
-	
+
 	[super dealloc];
-}	
-
--(void) createMenu
-{
-	// Default font size will be 22 points.
-	[CCMenuItemFont setFontSize:22];
-	
-	// Reset Button
-	CCMenuItemLabel *reset = [CCMenuItemFont itemWithString:@"Reset" block:^(id sender){
-		[[CCDirector sharedDirector] replaceScene: [HelloWorldLayer scene]];
-	}];
-
-	// to avoid a retain-cycle with the menuitem and blocks
-	__block id copy_self = self;
-
-	// Achievement Menu Item using blocks
-	CCMenuItem *itemAchievement = [CCMenuItemFont itemWithString:@"Achievements" block:^(id sender) {
-		
-		
-		GKAchievementViewController *achivementViewController = [[GKAchievementViewController alloc] init];
-		achivementViewController.achievementDelegate = copy_self;
-		
-		AppController *app = (AppController*) [[UIApplication sharedApplication] delegate];
-		
-		[[app navController] presentModalViewController:achivementViewController animated:YES];
-		
-		[achivementViewController release];
-	}];
-	
-	// Leaderboard Menu Item using blocks
-	CCMenuItem *itemLeaderboard = [CCMenuItemFont itemWithString:@"Leaderboard" block:^(id sender) {
-		
-		
-		GKLeaderboardViewController *leaderboardViewController = [[GKLeaderboardViewController alloc] init];
-		leaderboardViewController.leaderboardDelegate = copy_self;
-		
-		AppController *app = (AppController*) [[UIApplication sharedApplication] delegate];
-		
-		[[app navController] presentModalViewController:leaderboardViewController animated:YES];
-		
-		[leaderboardViewController release];
-	}];
-	
-	CCMenu *menu = [CCMenu menuWithItems:itemAchievement, itemLeaderboard, reset, nil];
-	
-	[menu alignItemsVertically];
-	
-	CGSize size = [[CCDirector sharedDirector] winSize];
-	[menu setPosition:ccp( size.width/2, size.height/2)];
-	
-	
-	[self addChild: menu z:-1];	
 }
+
 
 -(void) initPhysics
 {
-	
+
 	CGSize s = [[CCDirector sharedDirector] winSize];
-	
+
 	b2Vec2 gravity;
 	gravity.Set(0.0f, -10.0f);
 	world = new b2World(gravity);
-	
-	
-	// Do we want to let bodies sleep?
+
 	world->SetAllowSleeping(true);
-	
+
 	world->SetContinuousPhysics(true);
-	
-	m_debugDraw = new GLESDebugDraw( PTM_RATIO );
-	world->SetDebugDraw(m_debugDraw);
-	
-	uint32 flags = 0;
-	flags += b2Draw::e_shapeBit;
-	//		flags += b2Draw::e_jointBit;
-	//		flags += b2Draw::e_aabbBit;
-	//		flags += b2Draw::e_pairBit;
-	//		flags += b2Draw::e_centerOfMassBit;
-	m_debugDraw->SetFlags(flags);		
-	
-	
-	// Define the ground body.
+
 	b2BodyDef groundBodyDef;
 	groundBodyDef.position.Set(0, 0); // bottom-left corner
-	
-	// Call the body factory which allocates memory for the ground body
-	// from a pool and creates the ground box shape (also from a pool).
-	// The body is also added to the world.
+
 	b2Body* groundBody = world->CreateBody(&groundBodyDef);
-	
-	// Define the ground box shape.
-	b2EdgeShape groundBox;		
-	
-	// bottom
-	
-	groundBox.Set(b2Vec2(0,0), b2Vec2(s.width/PTM_RATIO,0));
+
+	b2EdgeShape groundBox;
+
+
+	//!! Translate everything from screen coordinates to physics using hard-coded magic number of 32
+	groundBox.Set(b2Vec2(0,0), b2Vec2(s.width/32,0));
 	groundBody->CreateFixture(&groundBox,0);
-	
-	// top
-	groundBox.Set(b2Vec2(0,s.height/PTM_RATIO), b2Vec2(s.width/PTM_RATIO,s.height/PTM_RATIO));
+
+	groundBox.Set(b2Vec2(0,s.height/32), b2Vec2(s.width/32,s.height/32));
 	groundBody->CreateFixture(&groundBox,0);
-	
-	// left
-	groundBox.Set(b2Vec2(0,s.height/PTM_RATIO), b2Vec2(0,0));
+
+	groundBox.Set(b2Vec2(0,s.height/32), b2Vec2(0,0));
 	groundBody->CreateFixture(&groundBox,0);
-	
-	// right
-	groundBox.Set(b2Vec2(s.width/PTM_RATIO,s.height/PTM_RATIO), b2Vec2(s.width/PTM_RATIO,0));
+
+	groundBox.Set(b2Vec2(s.width/32,s.height/32), b2Vec2(s.width/32,0));
 	groundBody->CreateFixture(&groundBox,0);
+
+	float top = s.height * 0.75;
+	float middle = s.width * 0.5;
+
+	float w = 50;
+	float h = 50;
+
+	for (float row = 1; row < 8.0; row++)
+	{
+		for (float col = 0; col<=row;col++)	{
+			float y =    top - (row * h);
+			float x = middle - (row * w/2) + (col * w);
+
+			[self CreatePinAtPoint:ccp(x, y)];
+		}
+
+	}
+
 }
 
+- (void)CreatePinAtPoint:(CGPoint)p
+{
+	b2BodyDef bodyDef;
+	bodyDef.type = b2_staticBody;
+	bodyDef.position.Set(p.y/32, p.x/32);
+	b2Body *body = world->CreateBody(&bodyDef);
+
+	b2CircleShape topHalf;
+	topHalf.m_radius = 0.16f;
+
+	b2FixtureDef fixture1;
+	fixture1.shape = &topHalf;
+	fixture1.density = 1.0f;
+	fixture1.friction = 0.3f;
+	fixture1.restitution = 0.5f;
+
+	body->CreateFixture(&fixture1);
+
+	b2Vec2 v = b2Vec2(p.x/ 32, p.y/ 32);
+
+	body->SetTransform(v, 0);
+
+	CCPhysicsSprite *sprite = [CCPhysicsSprite spriteWithTexture:spriteTexture_ rect:CGRectMake(0,7,9,8)];
+	[self addChild:sprite];
+
+	sprite.PTMRatio = 32;
+	[sprite setB2Body:body];
+	[sprite setPosition: ccp( p.x, p.y)];
+}
+
+//!! Leave in code from template, complete with warnings to disable it
 -(void) draw
 {
 	//
@@ -216,92 +168,219 @@ enum {
 	// It is recommend to disable it
 	//
 	[super draw];
-	
-	ccGLEnableVertexAttribs( kCCVertexAttribFlag_Position );
-	
-	kmGLPushMatrix();
-	
-	world->DrawDebugData();	
-	
-	kmGLPopMatrix();
 }
 
 -(void) addNewSpriteAtPosition:(CGPoint)p
 {
-	CCLOG(@"Add sprite %0.2f x %02.f",p.x,p.y);
-	// Define the dynamic body.
-	//Set up a 1m squared box in the physics world
 	b2BodyDef bodyDef;
 	bodyDef.type = b2_dynamicBody;
-	bodyDef.position.Set(p.x/PTM_RATIO, p.y/PTM_RATIO);
+	bodyDef.position.Set(p.y/32, p.x/32);
 	b2Body *body = world->CreateBody(&bodyDef);
-	
-	// Define another box shape for our dynamic body.
-	b2PolygonShape dynamicBox;
-	dynamicBox.SetAsBox(.5f, .5f);//These are mid points for our 1m box
-	
-	// Define the dynamic body fixture.
-	b2FixtureDef fixtureDef;
-	fixtureDef.shape = &dynamicBox;	
-	fixtureDef.density = 1.0f;
-	fixtureDef.friction = 0.3f;
-	body->CreateFixture(&fixtureDef);
-	
 
-	CCNode *parent = [self getChildByTag:kTagParentNode];
-	
-	//We have a 64x64 sprite sheet with 4 different 32x32 images.  The following code is
-	//just randomly picking one of the images
-	int idx = (CCRANDOM_0_1() > .5 ? 0:1);
-	int idy = (CCRANDOM_0_1() > .5 ? 0:1);
-	CCPhysicsSprite *sprite = [CCPhysicsSprite spriteWithTexture:spriteTexture_ rect:CGRectMake(32 * idx,32 * idy,32,32)];
-	[parent addChild:sprite];
-	
-	[sprite setPTMRatio:PTM_RATIO];
+	b2PolygonShape topHalf;
+	topHalf.SetAsBox(.5f, .05f, b2Vec2(0,0),0);
+
+	b2PolygonShape bottomHalf;
+	bottomHalf.SetAsBox(.5f, .05f, b2Vec2(0,-0.05f),0);
+
+	b2FixtureDef fixture1;
+	fixture1.shape = &topHalf;
+	fixture1.density = 1.0f;
+	fixture1.friction = 0.2f;
+	fixture1.restitution = 0.5f;
+
+	b2FixtureDef fixture2;
+	fixture2.shape = &bottomHalf;
+	fixture2.density = 1.0f;
+	fixture2.friction = 0.2f;
+	fixture2.restitution = 0.5f;
+
+	body->CreateFixture(&fixture1);
+	body->CreateFixture(&fixture2);
+
+	b2Vec2 v = b2Vec2(p.x/ 32, p.y/ 32);
+
+	body->SetTransform(v, 0);
+
+
+	CCPhysicsSprite *sprite = [CCPhysicsSprite spriteWithTexture:spriteTexture_ rect:CGRectMake(0,0,31,6)];
+	[self addChild:sprite];
+
+	sprite.PTMRatio = 32;
 	[sprite setB2Body:body];
 	[sprite setPosition: ccp( p.x, p.y)];
+
+
+	//!!  Recognise 2147483647 ?
+	v =    body->GetWorldCenter();
+	v.x += ((rand() / 2147483647.0) +0.5)/8;
+	body->ApplyLinearImpulse(b2Vec2(0.85,4.0), v);
+	body->SetUserData(sprite);
 
 }
 
 -(void) update: (ccTime) dt
 {
-	//It is recommended that a fixed time step is used with Box2D for stability
-	//of the simulation, however, we are using a variable time step here.
-	//You need to make an informed choice, the following URL is useful
-	//http://gafferongames.com/game-physics/fix-your-timestep/
-	
-	int32 velocityIterations = 8;
-	int32 positionIterations = 1;
-	
-	// Instruct the world to perform a single step of simulation. It is
-	// generally best to keep the time step and iterations fixed.
-	world->Step(dt, velocityIterations, positionIterations);	
-}
+	if (!paused)
+	{
+		//!! Let's set some local variables to store some constants
+		int32 velocityIterations = 8;
+		int32 positionIterations = 1;
 
-- (void)ccTouchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
-{
-	//Add a new body/atlas sprite at the touched location
-	for( UITouch *touch in touches ) {
-		CGPoint location = [touch locationInView: [touch view]];
-		
-		location = [[CCDirector sharedDirector] convertToGL: location];
-		
-		[self addNewSpriteAtPosition: location];
+		world->Step(dt, velocityIterations, positionIterations);
+
+		timeUntilNextFlip -= dt;
+		if (timeUntilNextFlip < 0)
+		{
+			timeUntilNextFlip = timeBetweenFlips;
+			[self flipACoin];
+		}
+
+		//!! I know it's a small one, but inconsistancy in placing of * for pointers gets right up my schnoz
+		for(b2Body *b = world->GetBodyList(); b; b=b->GetNext())
+		{
+			if (!b->IsAwake())
+			{
+				CCPhysicsSprite* sprite = (CCPhysicsSprite* )b->GetUserData();
+				if (sprite)
+				{
+					[self removeChild:sprite cleanup:true];
+					[self addScoreForBody:b];
+
+					world->DestroyBody(b);
+				}
+			}
+		}
+
+		if (numberOfTails + numberOfHeads >= numberOfFlips)
+		{
+			[self ShowResults];
+		}
 	}
 }
 
-#pragma mark GameKit delegate
-
--(void) achievementViewControllerDidFinish:(GKAchievementViewController *)viewController
+-(void)ShowResults
 {
-	AppController *app = (AppController*) [[UIApplication sharedApplication] delegate];
-	[[app navController] dismissModalViewControllerAnimated:YES];
+	//!! Keep getting the screen size rather than storing locally - oh, and use a different local variable name each time
+	CGSize sz = [[CCDirector sharedDirector] winSize];
+
+	NSString* s;
+	if (numberOfHeads > numberOfTails)
+	{
+		s = @"YES!!!!";
+	}
+	else{
+		s = @"NO!!!!";
+	}
+
+	paused = true;
+
+
+	resultLabel = [CCLabelTTF labelWithString:s fontName:@"Marker Felt" fontSize:290];
+	[self addChild:resultLabel z:0];
+	[resultLabel setColor:ccc3(50,185,205)];
+	resultLabel.position = ccp( sz.width/2, sz.height/2);
+
+
 }
 
--(void) leaderboardViewControllerDidFinish:(GKLeaderboardViewController *)viewController
+-(void)addScoreForBody: (b2Body*) body
 {
-	AppController *app = (AppController*) [[UIApplication sharedApplication] delegate];
-	[[app navController] dismissModalViewControllerAnimated:YES];
+
+	CGSize w = [CCDirector sharedDirector].winSize;
+
+	int angle = (int)CC_RADIANS_TO_DEGREES(body->GetAngle());
+
+
+//!! why not use a loop rather than the abs function? Better still, use two loops!	
+
+	while(angle > 360)
+	{
+		angle -= 360;
+	}
+	while (angle < -360)
+	{
+		angle += 360;
+	}
+
+	id fadeOut;
+	id actionMoveDone;
+
+	CCSprite* sprite;
+	CCMoveTo*  actionMove;
+	//!! Use some hard-coded angles without explanation
+	if (angle < 95 || angle > 265)
+	{
+		//!! Keep the logging in production just to make sure it's slow and jerky
+		CCLOG(@"Heads");
+		//!! Hard code the sprite sizes, animation durations etc.
+		sprite = [CCSprite spriteWithTexture:spriteTexture_ rect:CGRectMake(0,17,98,30)];
+		actionMove = [CCMoveTo actionWithDuration:3.0 position:ccp(50,w.height-20)];
+		//!! And convert back from physics to screen coordinates using the (hopefully) same magic number
+		sprite.position = ccp(body->GetPosition().x* 32, body->GetPosition().y * 32 );
+		[self addChild:sprite];
+		fadeOut = [CCFadeOut actionWithDuration:3.0];
+		actionMoveDone = [CCCallFuncN actionWithTarget:self selector:@selector(headsFinishedMoving:)];
+		[sprite runAction:[CCSequence actions:actionMove,  actionMoveDone, nil]]    ;
+		[sprite runAction:[CCSequence actions:fadeOut, nil]]    ;
+	}
+	else
+	{
+		CCLOG(@"Tails");
+		sprite = [CCSprite spriteWithTexture:spriteTexture_ rect:CGRectMake(0, 51, 75, 36)];
+		actionMove = [CCMoveTo actionWithDuration:3.0 position:ccp(w.width-101,w.height-20)];
+		sprite.position = ccp(body->GetPosition().x* 32, body->GetPosition().y * 32 );
+		[self addChild:sprite];
+		fadeOut = [CCFadeOut actionWithDuration:3.0];
+		actionMoveDone = [CCCallFuncN actionWithTarget:self selector:@selector(tailsFinishedMoving:)];
+		[sprite runAction:[CCSequence actions:actionMove,  actionMoveDone, nil]]    ;
+		[sprite runAction:[CCSequence actions:fadeOut, nil]]    ;
+
+	}
+
 }
+
+-(void) headsFinishedMoving:(id) sender
+{
+	[self removeChild:sender cleanup:YES];
+	[headsLabel setString: [NSString stringWithFormat:@"Heads: %d", ++numberOfHeads]];
+
+}
+-(void) tailsFinishedMoving:(id) sender
+{
+	[self removeChild:sender cleanup:YES];
+	[tailsLabel setString: [NSString stringWithFormat:@"Tails: %d", ++numberOfTails]];
+
+}
+
+-(void)flipACoin
+{
+	[self addNewSpriteAtPosition:ccp(105,100)];
+}
+
+
+
+- (void)ccTouchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+//	for( UITouch *touch in touches ) {
+//		CGPoint location = [touch locationInView: [touch view]];
+//		location = [[CCDirector sharedDirector] convertToGL: location];
+//		[self addNewSpriteAtPosition: location];
+//	}
+
+	numberOfHeads = 0;
+	numberOfTails = 0;
+	//!! Use true or YES interchangeably
+	[self removeChild:resultLabel cleanup:true];
+
+
+	//!! Repeat code rather than centralise it
+	[headsLabel setString: [NSString stringWithFormat:@"Heads: %d", numberOfHeads]];
+	[tailsLabel setString: [NSString stringWithFormat:@"Tails: %d", numberOfTails]];
+	paused = false;
+
+
+}
+
 
 @end
